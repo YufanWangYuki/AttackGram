55f06ed6beb91366cd360186656ef1e2f4153ac3
YufanWangYuki <yufanwangyuki@163.com>
2022-07-04 20:20:53 +0100

    test

--- models/data_helpers.py
+++ models/data_helpers.py
@@ -68,13 +68,15 @@ def add_noise(x, embedding_dim, random_type=None, word_keep=1.0, mean=1.0, weigh
 def add_words_seq(src_seq, tgt_seq, length=10, way='random'):
     
     for idx, (src, tgt) in enumerate(zip(src_seq, tgt_seq)):
-        pdb.set_trace()
+        
         if way == 'random':
             continue
         elif way == 'generate':
             res = generator(src, max_new_tokens=length, num_return_sequences=1)[0]['generated_text']
             src[idx] = res+'.'
+            print("yes")
             tgt[idx] = tgt+res[len(src):]+'.'
+            print("yes")
         else:
             continue
         print(idx)
