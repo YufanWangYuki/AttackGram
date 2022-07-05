3b77b29770ef8cb7254bec36b80fa20708a98853
YufanWangYuki <yufanwangyuki@163.com>
2022-07-05 14:08:05 +0100

    400

--- run_gen.sh
+++ run_gen.sh
@@ -15,12 +15,12 @@ export NUMEXPR_NUM_THREADS=1 # export NUMEXPR_NUM_THREADS=1
 
 # ------------------------ DIR --------------------------
 orig_path=/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written 
-# train_path_src=$orig_path/lib/gec-train-bpe-written/prep/train.src #3101262
-# train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/train.tgt
+train_path_src=$orig_path/lib/gec-train-bpe-written/prep/train.src #3101262
+train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/train.tgt
 
 # ----------------------- [debug] ---------------------------
-train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src #1929 
-train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
+# train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src #1929 
+# train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
 
 # ===================================================================================
 word_way=generate
@@ -29,9 +29,9 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/utils/dataset_generat
     --train_path_src $train_path_src \
 	--train_path_tgt $train_path_tgt \
     --log /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/$word_way/words$SGE_TASK_ID \
-    --start 0 \
-    --search_size 10 \
+    --start $SGE_TASK_ID \
+    --search_size 5 \
     --word_way $word_way
 
 # Run below command to submit this script as an array job
-# qsub -cwd -j yes -P esol -l qp=low -o LOGs/run-array-optimal.txt -t 1-400 -l not_host="air113|air112" run_gen.sh
+# qsub -cwd -j yes -P esol -l qp=low -o LOGs/run-array-optimal.txt -t 1-25 -l not_host="air113|air112" run_gen.sh
