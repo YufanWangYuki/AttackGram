8d583771ead49a3d3b9d6f5429826d5e2e288340
YufanWangYuki <yufanwangyuki@163.com>
2022-07-05 13:08:31 +0100

    gen

--- run_gen.sh
+++ run_gen.sh
@@ -28,7 +28,7 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/utils/dataset_generat
     --train_path_src $train_path_src \
 	--train_path_tgt $train_path_tgt \
     --log /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/words$SGE_TASK_ID.txt \
-    --start $SGE_TASK_ID \
+    --start 0 \
     --search_size 10 \
     --word_way $word_way
 
