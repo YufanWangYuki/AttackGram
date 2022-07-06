f7a6d39116ee0baf730ee783fbf3f2a935fcf72a
YufanWangYuki <yufanwangyuki@163.com>
2022-07-06 21:22:44 +0100

    count

--- get_best.sh
+++ get_best.sh
@@ -15,6 +15,8 @@ export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/p
 export PYTHONPATH="${PYTHONPATH}:/home/alta/BLTSpeaking/exp-yw575/GEC/NoiseGram/"
 
 for num in 2 3 4 5 6 7 8 9
+do
 $PYTHONBIN get_best.py \
     /home/alta/BLTSpeaking/exp-vr313/GEC/TunedGramformerAttack/universal_attack_logs/evade_perp_beam1/k${num}/ \
     /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/LOGs/best_words_${num}.txt
+done
