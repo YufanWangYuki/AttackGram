511674cadf9332a1060a4d531ed9acb6ecde1b1c
YufanWangYuki <yufanwangyuki@163.com>
2022-07-06 02:21:01 +0100

    6

--- run_gen.sh
+++ run_gen.sh
@@ -25,7 +25,7 @@ train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/train.tgt
 # ----------------------- [debug] ---------------------------
 # train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src #1929 
 # train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
-SGE_TASK_ID=5
+SGE_TASK_ID=6
 # ===================================================================================
 word_way=generate
 # mkdir /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/$word_way
@@ -48,7 +48,7 @@ $PYTHONBIN /home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/utils/dataset_generat
 # 2 6199190 qsub -cwd -j yes -o 'LOGs/job2.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
 # 3 6199191 qsub -cwd -j yes -o 'LOGs/job3.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
 # 4 6199192 qsub -cwd -j yes -o 'LOGs/job4.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
-# 5 6199191 qsub -cwd -j yes -o 'LOGs/job5.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
-# 6 6199191 qsub -cwd -j yes -o 'LOGs/job6.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
+# 5 6199193 qsub -cwd -j yes -o 'LOGs/job5.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
+# 6 6199194 qsub -cwd -j yes -o 'LOGs/job6.log' -P esol -l hostname='*' -l qp=cuda-low -l gpuclass='*' -l osrel='*' run_gen.sh 1 1
 
 
