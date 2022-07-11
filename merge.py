b713527bdfd256d9c1c8396b733aff1b035e5257
YufanWangYuki <yufanwangyuki@163.com>
2022-07-11 23:31:12 +0100

    prep

---
+++ merge.py
@@ -0,0 +1,12 @@
+file1 = '/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/merge/job_million_words0_src.txt'
+file2 = '/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/job_million_words1_src.txt'
+
+def merge(file1, file2):
+    f1 = open(file1, 'a+', encoding='utf-8')
+    with open(file2, 'r', encoding='utf-8') as f2:
+        f1.write('\n')
+        for i in f2:
+            f1.write(i)
+
+
+merge(file1, file2)
