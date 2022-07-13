094cb975078691b70a360b699c697c8c6fd6a22c
YufanWangYuki <yufanwangyuki@163.com>
2022-07-13 05:22:52 +0100

    0

--- train.sh
+++ train.sh
@@ -24,8 +24,8 @@ export PYTHONBIN=/home/alta/BLTSpeaking/exp-yw575/env/anaconda3/envs/gec37/bin/p
 # train_path_src=$orig_path/lib/gec-train-bpe-written/prep/train.src #3101262
 # train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/train.tgt
 orig_path=/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/merge
-train_path_src=$orig_path/old_len5_src.txt
-train_path_tgt=$orig_path/old_len5_tgt.txt
+train_path_src=$orig_path/old_len5_final_src.txt
+train_path_tgt=$orig_path/old_len5_final_tgt.txt
 dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src
 dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
 
@@ -67,19 +67,19 @@ loaddir='None'
 load_mode='null' # 'resume' | 'restart' | 'null'
 
 # ----------------------- [debug] ---------------------------
-# train_path_src=$orig_path/lib/gec-train-bpe-written/prep/dev.src #1929 
-# train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/dev.tgt
-# dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/toy.src
-# dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/toy.tgt
+train_path_src=$orig_path/lib/gec-train-bpe-written/prep/old_len5_toy_src.txt #1929 
+train_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/old_len5_toy_tgt.txt
+dev_path_src=$orig_path/lib/gec-train-bpe-written/prep/toy.src
+dev_path_tgt=$orig_path/lib/gec-train-bpe-written/prep/toy.tgt
 
 
 
 
-# # # num_epochs=2
-# minibatch_split=2
-# batch_size=4
-# checkpoint_every=100
-# print_every=2
+# # num_epochs=2
+minibatch_split=2
+batch_size=4
+checkpoint_every=100
+print_every=2
 
 # ----------------------- [noise] ---------------------------
 # ntype=Gaussian-adversarial #Gaussian, Bernoulli, Gaussian-adversarial, Adversarial
