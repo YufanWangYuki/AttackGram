e5c418b15208291ae16dbea5ea9c1de5676c93be
YufanWangYuki <yufanwangyuki@163.com>
2022-07-13 05:19:07 +0100

    0

--- merge.py
+++ merge.py
@@ -18,18 +18,10 @@ import pdb
 import re
 
 def load_sentences(path_src, path_tgt, start_idx=0, search_size=8000,mode="clean"):
-	with codecs.open(path_src, encoding='UTF-8') as f:
+	with open(path_src, encoding='UTF-8') as f:
 		src_sentences = f.readlines()
-	with codecs.open(path_tgt, encoding='UTF-8') as f:
+	with open(path_tgt, encoding='UTF-8') as f:
 		tgt_sentences = f.readlines()
-	rubbish = ['\n','\u202e','\u202d','\u202f','\u2028']
-	for src in src_sentences:
-		if src in rubbish:
-			src_sentences.remove(src)
-	
-	for tgt in tgt_sentences:
-		if tgt in rubbish:
-			tgt_sentences.remove(tgt)
 
 	
 	orig_path="/home/alta/BLTSpeaking/exp-ytl28/projects/gec-pretrained/exp-t5-written"
@@ -42,19 +34,22 @@ def load_sentences(path_src, path_tgt, start_idx=0, search_size=8000,mode="clean
 		'Mismatch src:tgt - {}:{}'.format(len(src_sentences),len(tgt_sentences))
 
 	num_sentences = len(src_sentences)
+	print(num_sentences)
 
+	src_sentences = [sentence.replace("\n","") for sentence in src_sentences]
+	tgt_sentences = [sentence.replace("\n","") for sentence in tgt_sentences]
+	values = set()
 	if mode=="clean":
 		# pdb.set_trace()
 
-		src_sentences = [sentence.replace("\n","") for sentence in src_sentences]
-		tgt_sentences = [sentence.replace("\n","") for sentence in tgt_sentences]
+		
 		src_sentences_orig = [sentence.replace("\n","") for sentence in src_sentences_orig]
 		tgt_sentences_orig = [sentence.replace("\n","") for sentence in tgt_sentences_orig]
 
 		res_src = []
 		res_tgt = []
 		count = 0
-		values = set()
+		
 
 
 		# print(num_sentences)
@@ -88,9 +83,7 @@ def load_sentences(path_src, path_tgt, start_idx=0, search_size=8000,mode="clean
 		test_src = src_sentences[start_index:end_index]
 		test_tgt = tgt_sentences[start_index:end_index]
 
-		src_seqs = [sentence.strip() for sentence in test_src]
-		tgt_seqs = [sentence.strip() for sentence in test_tgt]
-		pdb.set_trace()
+		
     
 	return res_src, res_tgt, values
 
@@ -103,17 +96,6 @@ def merge(file1, file2):
         for i in f2:
             f1.write(i)
 
-def sample(file1, file2):
-    f1 = open(file1, 'r')
-    with open(file2, 'w+') as f2:
-        for i in f1[:200]:
-            f2.write(i)
-            f2.write('\n')
-
-def clean(file):
-    with open(file, 'r') as f2:
-        if f2:
-            print("None")
 
 
 dir = "/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/merge/"
@@ -137,25 +119,25 @@ dir = "/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/merge/"
 
 src=dir+"old_len5_src.txt"
 tgt=dir+"old_len5_tgt.txt"
-src_seqs, tgt_seqs, gens = load_sentences(src, tgt, 0, 200,"clean")
+src_seqs, tgt_seqs, gens = load_sentences(src, tgt, 0, 200,"sample")
 outdir="/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/generate/merge/"
 
-with open(outdir+"old_len5_final_src.txt", 'w+') as f:
+with open(outdir+"old_len5_toy_src.txt", 'w+') as f:
     for seq in src_seqs:
         f.write(seq)
         f.write('\n')
 f.close()
 
 
-with open(outdir+"old_len5_final_tgt.txt", 'w+') as f:
+with open(outdir+"old_len5_toy_tgt.txt", 'w+') as f:
     for seq in tgt_seqs:
         f.write(seq)
         f.write('\n')
 f.close()
 
-with open(outdir+"old_len5_final_gens.txt", 'w+') as f:
-    for seq in gens:
-        f.write(seq)
-        f.write('\n')
-f.close()
+# with open(outdir+"old_len5_final_gens.txt", 'w+') as f:
+#     for seq in gens:
+#         f.write(seq)
+#         f.write('\n')
+# f.close()
 
