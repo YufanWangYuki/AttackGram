fbae657a7512603cb1f9bd282f4843a5e50f4fd3
YufanWangYuki <yufanwangyuki@163.com>
2022-07-24 22:51:57 +0100

    merge

--- run_gen_perp.py
+++ run_gen_perp.py
@@ -95,11 +95,12 @@ if __name__ == "__main__":
     res = []
     tries = []
     founded = 0
-    with open("/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/random_valid/2_100_2.txt", 'r') as f:
-        lines = f.readlines()[1:]
+    with open("/home/alta/BLTSpeaking/exp-yw575/GEC/AttackGram/dataset/random_valid/2_100_2.txt", 'r') as f2:
+        lines = f2.readlines()[1:]
         for line in lines:
             temp = line.split(" ")[:-1]
             tries.append(temp)
+    f2.close()
     founded = len(tries)   
     pdb.set_trace()
 
